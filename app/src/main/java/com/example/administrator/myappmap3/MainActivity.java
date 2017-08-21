eae3528dac9721e5dfdfe2187384eb1baec00716
Ducksion <rsuey@sina.com>
2017-08-21 18:57:10 +0800

    1 map display

---
+++ app/src/main/java/com/example/administrator/myappmap3/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.administrator.myappmap3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+import com.baidu.location.BDLocation;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(getApplicationContext());  //ssss
+        setContentView(R.layout.activity_main);
+    }
+}
