8882aed743b731620d55458bcaea76e0209171d9
Ducksion <rsuey@sina.com>
2017-08-22 10:09:23 +0800

    add button click

--- app/src/main/java/com/example/administrator/myappmap3/MainActivity.java
+++ app/src/main/java/com/example/administrator/myappmap3/MainActivity.java
@@ -14,7 +14,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SDKInitializer.initialize(getApplicationContext()); 
+        SDKInitializer.initialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+    }
+
+    public void go_click(View view){
+        MapView mymapView;
+        BaiduMap myBaiduMap;
+
+        // 地图初始化
+        mymapView = (MapView)view.findViewById(R.id.mymapView);
+        myBaiduMap = mymapView.getMap();
     }
 }
