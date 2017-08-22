28f62d1bb6e2c1bc80eee27b7190c150b98854e9
Ducksion <rsuey@sina.com>
2017-08-22 13:42:05 +0800

    Map Can dingwei V1

--- app/src/main/java/com/example/administrator/myappmap3/MainActivity.java
+++ app/src/main/java/com/example/administrator/myappmap3/MainActivity.java
@@ -3,14 +3,24 @@ package com.example.administrator.myappmap3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.content.Context;
+import android.content.pm.*;
+import android.support.v4.content.*;
+import android.location.Location;
+import android.location.LocationManager;
 import android.widget.*;
 import com.baidu.location.BDLocation;
 import com.baidu.mapapi.SDKInitializer;
 import com.baidu.mapapi.map.BaiduMap;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.search.district.*;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MyLocationConfiguration;
 
 public class MainActivity extends AppCompatActivity {
     MapView mymapView = null;
+    Location location;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //获取地图控件引用
         mymapView = (MapView) findViewById(R.id.mymapView);
+        location = getLocation(this);
     }
 
     @Override
@@ -39,7 +50,36 @@ public class MainActivity extends AppCompatActivity {
         mymapView.onPause();
     }
 
-    public void go_click(View view){
+    //Get the Location by GPS or WIFI
+    public Location getLocation(Context context) {
 
+        if (ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission( context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return null ;
+        }
+        LocationManager locMan = (LocationManager) context
+                .getSystemService(Context.LOCATION_SERVICE);
+        if (location == null) {
+            location = locMan.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        return location;
+    }
+
+    public void go_click(View view){
+        BaiduMap oMap;
+        oMap = mymapView.getMap();
+        oMap.setMyLocationEnabled(true);
+        // 构造定位数据
+        MyLocationData locData = new MyLocationData.Builder()
+                                    .accuracy(location.getAccuracy())
+                                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                                    .direction(100).latitude(location.getLatitude())
+                                    .longitude(location.getLongitude()).build();
+        // 设置定位数据
+        oMap.setMyLocationData(locData);
+        // 设置定位图层的配置（定位模式，是否允许方向信息，用户自定义定位图标）
+        MyLocationConfiguration config = new MyLocationConfiguration(MyLocationConfiguration.LocationMode.NORMAL, true, null);
+        oMap.setMyLocationConfiguration(config);
     }
 }
